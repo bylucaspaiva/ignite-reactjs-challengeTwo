2af556877f56de48feff0c6a94c052b69afa880b
bylucaspaiva <lucasp4iva@gmail.com>
2022-03-16 20:31:06 -0300

    feat: adding context to components to interface make sense

--- src/App.tsx
+++ src/App.tsx
@@ -20,22 +20,11 @@ interface GenreResponseProps {
 }
 
 
-
 export function App() {
   const [selectedGenreId, setSelectedGenreId] = useState(1);
-
-  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
-
-  
   const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
 
   useEffect(() => {
-    api.get<GenreResponseProps[]>('genres').then(response => {
-      setGenres(response.data);
-    });
-  }, []);
-
-
     api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
       setSelectedGenre(response.data);
     })
@@ -47,10 +36,8 @@ export function App() {
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <SideBar />
-      <Content />
-
-    
+      <SideBar selectedGenreId={selectedGenreId} handleClickButton={handleClickButton}/>
+      <Content selectedGenre={selectedGenre} selectedGenreId={selectedGenreId}/>
     </div>
   )
 }
